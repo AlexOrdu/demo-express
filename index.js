6204208f8ef3a316200013ddee9ea877321286a9
AlexOrdu <alexmaxol06@gmail.com>
2017-02-16 20:54:42 -0700

    corrección de errores y se agregan lindeas en index.pug

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ var express = require('express');
 var app = express();
 app.set("view engine", "pug");
 
-
 app.get('/', function(req, res) {
-var nombre = new object();
-nombre.primerNombre = "Alejandro";
+  var nombre = new Object();
+  nombre.primerNombre = "Alejandro ";
+  nombre.apellido = "Orduño";
 
-  res.render("index", {nombre:nombre});
+  var numero1 = 4;
+  var numero2 = 8;
+
+  var paises = ["México", "China", "Canada", "Papua Nueva Guinea"];
+
+
+  res.render("index", {nombre:nombre, numero1:numero1,
+     numero2:numero2, paises:paises});
 });
 
 app.listen(8080);
